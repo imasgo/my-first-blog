8ab30584ce294dc07d283c57afa2cced987dafbc
imasgo <sasmashka@gmail.com>
2018-02-13 16:16:47 +0300

    my django app first commit

---
+++ testsite/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import HistoryNote
+
+admin.site.register(HistoryNote)
---
+++ testsite/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone 
+
+class HistoryNote(models.Model):
+	author = models.ForeignKey('auth.User')
+	name_in_sources = models.CharField(max_length = 250)
+	titles = models.TextField()
+	life_dates = models.CharField(max_length = 100)
+	biography = models.TextField()
+	family_relationship = models.TextField()
+	others = models.TextField()
+	sources = models.TextField()
+	literature = models.TextField()
+
+
+def publish(self):
+	self.save()
+
+def __str__(self):
+	return self.title	
+
+
